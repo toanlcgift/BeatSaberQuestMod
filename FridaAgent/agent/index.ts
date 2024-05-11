49e8a38aeae1e78336772940409172bfed48761d
toanlcgift <toanlcgift@gmail.com>
2024-05-11 18:52:54 +0700

    update frida agent & fix build agent from visual studio

--- FridaAgent/agent/index.ts
+++ FridaAgent/agent/index.ts
@@ -1,5 +1,21 @@
+import "frida-il2cpp-bridge";
 
 console.log("start");
+
+const classes = ["BeatSaberTest"];
+const methods = [".cctor"]
+
+Il2Cpp.perform(() => {
+    console.log(Il2Cpp.unityVersion);
+    setTimeout(() => {
+        Il2Cpp.trace()
+            .assemblies(Il2Cpp.domain.assembly("Class1"))
+            .filterClasses(cls => classes.includes(cls.name))
+            .filterMethods(mtd => methods.includes(mtd.name))
+            .and()
+            .attach();
+    }, 30 * 1000);
+});
 
 
 
