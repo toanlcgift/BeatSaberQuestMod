d46e36d11889a5acdb9bcb4b51b5e341aab5dab4
toanlcgift <toanlcgift@gmail.com>
2024-05-13 14:47:16 +0700

    update

--- FridaAgent/agent/index.ts
+++ FridaAgent/agent/index.ts
@@ -1,31 +1,22 @@
-import "./indexlib.js";
+import "frida-il2cpp-bridge";
 
 console.log("start");
 
 Il2Cpp.perform(() => {
     console.log(Il2Cpp.unityVersion);
 
-    Il2Cpp.domain.assemblies.map(x => x.name);
+    console.log(Il2Cpp.domain.assemblies.map(x => x.name));
 
-    const class1lib = Il2Cpp.domain.assembly("ClassLibrary1").image;
-    const mainClass = class1lib.class("ClassLibrary1.MainClass");
-    const mainClassConstructor = mainClass.method<Il2Cpp.Object>(".cctor");
+    const mainlib = Il2Cpp.domain.assembly("Main").image;
+    const MainMenuViewController = mainlib.class("MainMenuViewController");
+    const HandleMenuButton = MainMenuViewController.method("HandleMenuButton");
 
-    const newMainClassObj = mainClassConstructor.invoke();
-    newMainClassObj.method("Main").invoke();
-	
-	console.log("Invoked Main() method");
-
-    
-    //setTimeout(() => {
-    //    Il2Cpp.trace()
-    //        .assemblies(Il2Cpp.domain.assembly("Class1"))
-    //        .filterClasses(cls => classes.includes(cls.name))
-    //        .filterMethods(mtd => methods.includes(mtd.name))
-    //        .and()
-    //        .attach();
-    //}, 30 * 1000);
-}, undefined, "libclass1.so");
+    // private void HandleMenuButton(MainMenuViewController.MenuButton menuButton)
+    HandleMenuButton.implementation = function (menuButtonEnum: number): void
+    {
+        console.log("menu button clicked = " + menuButtonEnum);
+    };
+});
 
 
 
