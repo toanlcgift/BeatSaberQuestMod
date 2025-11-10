657ee20bc4ce9db76a215393530fc9f8c3a265ab
toanlcgift <toanlcgift@gmail.com>
2025-11-10 23:38:15 +0700

    update

--- FridaAgent/agent/index.ts
+++ FridaAgent/agent/index.ts
@@ -29,18 +29,52 @@ Il2Cpp.perform(() => {
         console.log("note missed but hacked");
     }
 
-    const SaberModelContainer = mainlib.class("SaberModelContainer");
-    console.log(SaberModelContainer.methods);
-    const Awake = SaberModelContainer.method("Start");
-    Awake.implementation = function (): void {
-        console.log("Start called");
+    const SetSaberGlowColor = mainlib.class("SetSaberGlowColor");
+    console.log(SetSaberGlowColor.methods);
+    const SetColors = SetSaberGlowColor.method("SetColors");
+    SetColors.implementation = function (): void {
+        console.log("SetColors called");
     };
 
-    Il2Cpp.trace()
-        .assemblies(Il2Cpp.domain.assembly("Main"))
-        .filterClasses((klass) => klass.name.endsWith("SetSaberGlowColor"))
-        .and()
-        .attach();
+    const SetSaberFakeGlowColor = mainlib.class("SetSaberFakeGlowColor");
+    console.log(SetSaberFakeGlowColor.methods);
+    const SetColors2 = SetSaberFakeGlowColor.method("SetColors");
+    SetColors2.implementation = function (): void {
+        console.log("SetColors SetSaberFakeGlowColor called");
+    };
+
+    const SaberModelController = mainlib.class("SaberModelController");
+    console.log(SaberModelController.methods);
+
+    const unityEngine = Il2Cpp.domain.assembly("UnityEngine.CoreModule").image;
+    const Color = unityEngine.class("UnityEngine.Color");
+    const colorWrap = Color.alloc();
+    colorWrap.field("r").value = 245;
+    colorWrap.field("g").value = 40;
+    colorWrap.field("b").value = 145;
+    colorWrap.field("a").value = 1.0;
+    console.log("Color created: " + colorWrap);
+
+    const Init = SaberModelController.method("Init");
+    Init.implementation = function (parent: Il2Cpp.Parameter.Type, saber: Il2Cpp.Parameter.Type, trailTintColor: Il2Cpp.Parameter.Type): void {
+        console.log("SaberModelController Init called" + parent + saber + colorWrap.unbox() + trailTintColor);
+        this.method("Init").invoke(parent, saber, colorWrap.unbox());
+    }
+
+    const SaberBurnMarkArea = mainlib.class("SaberBurnMarkArea");
+    console.log(SaberBurnMarkArea.methods);
+    const Start = SaberBurnMarkArea.method("Start");
+    Start.implementation = function (): void {
+        console.log("SaberBurnMarkArea Start called");
+        //this.method("Start").invoke();
+    }
+
+    //Il2Cpp.trace()
+    //    .assemblies(Il2Cpp.domain.assembly("Main"))
+    //    .filterClasses((klass) => klass.name === "Saber")
+    //    .filterMethods((method) => method.name === "ManualUpdate" || method.name === "OnDrawGizmos")
+    //    .and()
+    //    .attach();
 });
 
 
